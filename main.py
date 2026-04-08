f6141fda79618887cc21599fecd881dd8e8ef323
mohamed666999 <nimomido07@gmail.com>
2026-04-08 10:45:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ from openai import OpenAI
 
 # ==================== الإعدادات ====================
 TOKEN           = os.environ.get("TELEGRAM_TOKEN",  "8706937528:AAHVug63kujbf2t2ntKiQzpa3IN6Wr5b16
-SUPABASE_URL    = os.environ.get("SUPABASE_URL",    "https://mamjpudfwhmvqdvrqojb.supabase.co")
-SUPABASE_KEY    = os.environ.get("SUPABASE_KEY",    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbWpwdWRmd2htdnFkdnJxb2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMTAwNjMsImV4cCI6MjA5MDc4NjA2M30.Y6tajMxbkCgcOx8tQIowg6LjxfjaRrnBAO9DwqZCVLI")
-SUPABASE_DB_URL = os.environ.get("SUPABASE_DB_URL", "postgresql://postgres.mamjpudfwhmvqdvrqojb:Loploplop909090.@aws-0-eu-west-1.pooler.supabase.com:6543/postgres")
+SUPABASE_URL    = os.environ.get("SUPABASE_URL",    https://mamjpudfwhmvqdvrqojb.supabase.co")
+SUPABASE_KEY    = os.environ.get("SUPABASE_KEY",    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbWpwdWRmd2htdnFkdnJxb2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMTAwNjMsImV4cCI6MjA5MDc4NjA2M30.Y6tajMxbkCgcOx8tQIowg6LjxfjaRrnBAO9DwqZCVLI")
+SUPABASE_DB_URL = os.environ.get("SUPABASE_DB_URL", postgresql://postgres.mamjpudfwhmvqdvrqojb:Loploplop909090.@aws-0-eu-west-1.pooler.supabase.com:6543/postgres")
 ADMIN_ID        = 6033203084
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
