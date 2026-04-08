3774110bfa29b61b93554318a055d249ef5ba428
mohamed666999 <nimomido07@gmail.com>
2026-04-08 10:44:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ import aiohttp
 from openai import OpenAI
 
 # ==================== الإعدادات ====================
-TOKEN           = os.environ.get("TELEGRAM_TOKEN",  "8706937528:AAHVug63kujbf2t2ntKiQzpa3IN6Wr5b16s")
+TOKEN           = os.environ.get("TELEGRAM_TOKEN",  "8706937528:AAHVug63kujbf2t2ntKiQzpa3IN6Wr5b16
 SUPABASE_URL    = os.environ.get("SUPABASE_URL",    "https://mamjpudfwhmvqdvrqojb.supabase.co")
 SUPABASE_KEY    = os.environ.get("SUPABASE_KEY",    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbWpwdWRmd2htdnFkdnJxb2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMTAwNjMsImV4cCI6MjA5MDc4NjA2M30.Y6tajMxbkCgcOx8tQIowg6LjxfjaRrnBAO9DwqZCVLI")
 SUPABASE_DB_URL = os.environ.get("SUPABASE_DB_URL", "postgresql://postgres.mamjpudfwhmvqdvrqojb:Loploplop909090.@aws-0-eu-west-1.pooler.supabase.com:6543/postgres")
